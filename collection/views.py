7223889b67ac715b34e2d8e0478d00acc11784af
Tracy Osborn <tracy@weddinglovely.com>
2014-12-26 17:19:34 -0800

    End of Chapter 7

--- collection/views.py
+++ collection/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
+from collection.models import Thing
 
-
+# the rewritten view!
 def index(request):
-    number = 6
-    # don't forget the quotes because it's a string, not an integer
-    thing = "Thing name"
+    things = Thing.objects.all()
     return render_to_response('index.html', {
-        'number': number,
-        # don't forget to pass it in, and the last comma
-        'thing': thing,
+        'things': things,
     }, context_instance=RequestContext(request))
