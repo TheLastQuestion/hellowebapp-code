0f9216101bffef05ad3541ff882b222aa0f24b71
Tracy Osborn <tracy@weddinglovely.com>
2014-12-26 18:52:15 -0800

    added space

--- hellowebapp/wsgi.py
+++ hellowebapp/wsgi.py
@@ -2,5 +2,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellowebapp.settings_production")
 application = Cling(get_wsgi_application())
